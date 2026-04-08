5367b0c32ec786783542de88b5db8a8b46149329
Yazan <yazanziyad98@gmail.com>
2026-04-08 23:26:50 +0300

    adding minio credintials

--- Gov_DWH_Pipeline.py
+++ Gov_DWH_Pipeline.py
@@ -2,3 +2,16 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import * 
 from pyspark.sql.functions import col
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+ 
+
+load_dotenv() 
+
+
+
+MINIO_ACCESS = os.getenv("MINIO_ACCESS")
+MINIO_SECRET = os.getenv("MINIO_SECRET")
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+
+print(MINIO_ACCESS)
