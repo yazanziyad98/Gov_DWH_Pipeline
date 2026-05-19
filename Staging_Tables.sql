9f3422894af018f06c05d053406f7061bd4347cc
Yazan <yazanziyad98@gmail.com>
2026-05-19 10:09:16 +0300

    Update tables schemas

--- Staging_Tables.sql
+++ Staging_Tables.sql
@@ -1,92 +1,75 @@
-GO
-CREATE TABLE [dbo].[SSC_Salaries](
-	[Source_Type_Code] [numeric](1, 0) NOT NULL,
-	[Source_Type_Name] [nvarchar](100) NOT NULL,
-	[Work_Type] [nvarchar](100) NOT NULL,
-	[DType] [nvarchar](100) NOT NULL,
-	[National_Numer] [nvarchar](10) NOT NULL,
-	[Social_Security_Number] [numeric](10, 0) NOT NULL,
-	[Personal_Number] [numeric](10, 0) NULL,
-	[Card_Number] [numeric](10, 0) NULL,
-	[Salary] [numeric](10, 3) NULL,
-	[Last_Subscription_Date] [date] NULL,
-	[Pension_Type] [nvarchar](200) NULL,
-	[Pension_Active] [nvarchar](10) NULL,
-	[Sector] [nvarchar](50) NULL,
-	[Retired_From_Facility_Name] [nvarchar](750) NULL,
-	[Agent_Name] [nvarchar](300) NULL,
-	[Last_Active_Subscription_Date] [date] NULL,
-	[Last_Active_subscription_Count] [numeric](5, 0) NULL,
-	[First_Subscription_Date] [date] NULL,
-	[Total_Subscriptions_Count] [numeric](5, 0) NULL,
-	[RATDISB] [numeric](10, 3) NULL,
-	[Workplace_Name] [nvarchar](600) NULL,
-	[Workplace_National_Number] [numeric](10, 0) NULL,
-	[Sector_Code] [numeric](2, 0) NULL,
-	[Sub_Sector_Code] [numeric](4, 0) NULL,
-	[Branch_Code] [numeric](2, 0) NULL
-) ON [PRIMARY]
-GO
-
-
-
-
-GO
-	CREATE TABLE [dbo].[CSPD_Personal_Info](
-		[National_Number] [nvarchar](10) NOT NULL,
-		[First_Name] [nvarchar](30) NULL,
-		[Second_Name] [nvarchar](20) NULL,
-		[Third_Name] [nvarchar](20) NULL,
-		[Family_Name] [nvarchar](20) NULL,
-		[Gender] [nvarchar](1) NULL,
-		[Religion_Code] [nvarchar](2) NULL,
-		[Social_Status_Code] [nvarchar](2) NULL,
-		[Nationality_Code] [nvarchar](3) NULL,
-		[Birth_Country_Code] [nvarchar](3) NULL,
-		[Birth_Governorate_Code] [nvarchar](2) NULL,
-		[Birth_Liwa_Code] [nvarchar](2) NULL,
-		[Birth_Kada_Code] [nvarchar](1) NULL,
-		[Birth_City_Code] [nvarchar](3) NULL,
-		[Birth_Date] [date] NULL,
-		[Father_National_Number] [nvarchar](10) NULL,
-		[Mother_National_Number] [nvarchar](10) NULL,
-		[Civil_Reg_Office_Code] [nvarchar](3) NULL,
-		[Civil_Registration_Number] [nvarchar](7) NULL,
-		[Family_Book_Pub_Office_Code] [nvarchar](3) NULL,
-		[Family_Book_Number] [nvarchar](7) NULL,
-		[Family_Book_Issue_Date] [date] NULL,
-		[Identity_Pub_Office_Code] [nvarchar](3) NULL,
-		[Identity_Number] [nvarchar](8) NULL,
-		[Identity_Issue_Date] [date] NULL,
-		[Identity_Expired_Date] [date] NULL,
-		[Passport_Pub_Office_Code] [nvarchar](3) NULL,
-		[Passport_Number] [nvarchar](8) NULL,
-		[Passport_Issue_date] [date] NULL,
-		[Passport_Expired_date] [date] NULL,
-		[Passport_File_Office_Code] [nvarchar](3) NULL,
-		[Passport_File_Number] [nvarchar](20) NULL,
-		[IS_Alive] [nvarchar](1) NULL,
-		[FAM_STS] [nvarchar](2) NULL,
-		[First_Name_En] [nvarchar](30) NULL,
-		[Second_Name_En] [nvarchar](20) NULL,
-		[Third_Name_En] [nvarchar](20) NULL,
-		[Family_Name_En] [nvarchar](20) NULL,
-		[Mother_Name] [nvarchar](30) NULL
-	) ON [PRIMARY]
-GO
+  CREATE TABLE `salaries` (
+  `Source_Type_Code` int NOT NULL,
+  `Source_Type_Name` varchar(100) NOT NULL,
+  `Work_Type` varchar(100) NOT NULL,
+  `DType` varchar(100) NOT NULL,
+  `National_Number` varchar(10) NOT NULL,
+  `Social_Security_Number` decimal(10,0) NOT NULL,
+  `Personal_Number` decimal(10,0) DEFAULT NULL,
+  `Card_Number` decimal(10,0) DEFAULT NULL,
+  `Salary` decimal(10,3) DEFAULT NULL,
+  `Last_Subscription_Date` date DEFAULT NULL,
+  `Pension_Type` varchar(200) DEFAULT NULL,
+  `Pension_Active` varchar(10) DEFAULT NULL,
+  `Sector` varchar(50) DEFAULT NULL,
+  `Retired_From_Facility_Name` varchar(750) DEFAULT NULL,
+  `Agent_Name` varchar(300) DEFAULT NULL,
+  `Last_Active_Subscription_Date` date DEFAULT NULL,
+  `Last_Active_subscription_Count` decimal(5,0) DEFAULT NULL,
+  `First_Subscription_Date` date DEFAULT NULL,
+  `Total_Subscriptions_Count` decimal(5,0) DEFAULT NULL,
+  `RATDISB` decimal(10,3) DEFAULT NULL,
+  `Workplace_Name` varchar(600) DEFAULT NULL,
+  `Workplace_National_Number` decimal(10,0) DEFAULT NULL,
+  `Sector_Code` decimal(2,0) DEFAULT NULL,
+  `Sub_Sector_Code` decimal(4,0) DEFAULT NULL,
+  `Branch_Code` decimal(2,0) DEFAULT NULL,
+  `Load_Date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
 
 
-
-
-
-CREATE TABLE [dbo].[SSC_Insured_Info](
-	[Social_Security_Number] [numeric](10, 0) NOT NULL,
-	[National_Number] [nvarchar](10) NULL,
-	[Personal_Number] [numeric](10, 0) NULL
-) ON [PRIMARY]
+CREATE TABLE `personal_info` (
+  `National_Number` varchar(10) NOT NULL,
+  `First_Name` varchar(30) DEFAULT NULL,
+  `Second_Name` varchar(20) DEFAULT NULL,
+  `Third_Name` varchar(20) DEFAULT NULL,
+  `Family_Name` varchar(20) DEFAULT NULL,
+  `Gender` varchar(1) DEFAULT NULL,
+  `Religion_Code` varchar(2) DEFAULT NULL,
+  `Social_Status_Code` varchar(2) DEFAULT NULL,
+  `Nationality_Code` varchar(3) DEFAULT NULL,
+  `Birth_Country_Code` varchar(10) DEFAULT NULL,
+  `Birth_Governorate_Code` varchar(2) DEFAULT NULL,
+  `Birth_Liwa_Code` varchar(2) DEFAULT NULL,
+  `Birth_Kada_Code` varchar(1) DEFAULT NULL,
+  `Birth_City_Code` varchar(10) DEFAULT NULL,
+  `Birth_Date` date DEFAULT NULL,
+  `Father_National_Number` varchar(10) DEFAULT NULL,
+  `Mother_National_Number` varchar(10) DEFAULT NULL,
+  `Civil_Reg_Office_Code` varchar(3) DEFAULT NULL,
+  `Civil_Registration_Number` varchar(7) DEFAULT NULL,
+  `Family_Book_Pub_Office_Code` varchar(3) DEFAULT NULL,
+  `Family_Book_Number` varchar(7) DEFAULT NULL,
+  `Family_Book_Issue_Date` date DEFAULT NULL,
+  `Identity_Pub_Office_Code` varchar(3) DEFAULT NULL,
+  `Identity_Number` varchar(8) DEFAULT NULL,
+  `Identity_Issue_Date` date DEFAULT NULL,
+  `Identity_Expired_Date` date DEFAULT NULL,
+  `Passport_Pub_Office_Code` varchar(3) DEFAULT NULL,
+  `Passport_Number` varchar(8) DEFAULT NULL,
+  `Passport_Issue_date` date DEFAULT NULL,
+  `Passport_Expired_date` date DEFAULT NULL,
+  `Passport_File_Office_Code` varchar(3) DEFAULT NULL,
+  `Passport_File_Number` varchar(20) DEFAULT NULL,
+  `IS_Alive` varchar(1) DEFAULT NULL,
+  `FAM_STS` varchar(2) DEFAULT NULL,
+  `Mother_Name` varchar(30) DEFAULT NULL,
+  `Birth_Country` varchar(10) DEFAULT NULL,
+  `Load_Date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
@@ -95,11 +78,13 @@ CREATE TABLE [dbo].[SSC_Insured_Info](
 
 
 
-CREATE TABLE [dbo].[SSC_Insured_Yearly_Salary](
-	[Social_Security_Number] [numeric](10, 0) NOT NULL,
-	[Salary_Year] [numeric](4, 0) NOT NULL,
-	[Salary_Amount] [numeric](9, 3) NULL
-) ON [PRIMARY]
+
+ CREATE TABLE `insured_info` (
+  `Social_Security_Number` decimal(10,0) NOT NULL,
+  `National_Number` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `Personal_Number` decimal(10,0) DEFAULT NULL,
+  `Load_Date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
@@ -107,9 +92,26 @@ CREATE TABLE [dbo].[SSC_Insured_Yearly_Salary](
 
 
 
-CREATE TABLE [dbo].[SSC_Insured_Transaction](
-	[Social_Security_Number] [numeric](10, 0) NOT NULL,
-	[Start_Date] [date] NOT NULL,
-	[End_Date] [date] NULL,
-	[Salary_Amount] [numeric](9, 3) NULL
-) ON [PRIMARY]
+
+
+CREATE TABLE `insured_yearly_salary` (
+  `Social_Security_Number` decimal(10,0) NOT NULL,
+  `Salary_Year` decimal(4,0) NOT NULL,
+  `Salary_Amount` decimal(9,1) DEFAULT NULL,
+  `Load_Date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+
+
+
+ CREATE TABLE `insured_transaction` (
+  `Social_Security_Number` decimal(10,0) NOT NULL,
+  `Start_Date` date NOT NULL,
+  `End_Date` date DEFAULT NULL,
+  `Salary_Amount` decimal(9,1) DEFAULT NULL,
+  `Load_Date` datetime DEFAULT NULL,
+  `message_num` int DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
